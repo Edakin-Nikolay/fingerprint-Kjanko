81d1edc3420108413da4037e391c29d81f153f49
Nikolay <nikswat@mail.ru>
2019-04-24 17:19:11 +0700

    [WIP] попытка распараллеливания

--- app.py
+++ app.py
@@ -3,28 +3,26 @@ import cv2
 
 import create_users
 import comparing
+import time
+import multiprocessing
 
-os.chdir("C:/Institut/programs/")
-# os.chdir("/home/nikolay/learning/fingerprint-Kjanko")
-
-
-def main():
-    print("run comparing...")
-    list_probe = create_users.make("fingers/probe/crossMatchDB")
-    print("created list of probe...")
-    list_standard = create_users.make("fingers/standard/crossMatchDB")
-    print("created list of standard...")
-
-    list_results = list(map(lambda probe: comparing.compare_one_with_n(probe, list_standard), list_probe))
-    print("list", list(map(lambda res: res.to_string(), list_results)))
-    print(comparing.fold_results(list_results).to_string())
-    # probe = cv2.imread("database/101_2.tif", cv2.IMREAD_GRAYSCALE)
-    # standard = cv2.imread("database/101_3.tif", cv2.IMREAD_GRAYSCALE)
-    # comparing.compare(probe, standard)
+# os.chdir("C:/Institut/programs/")
+os.chdir("/home/nikolay/learning/")
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except:
-        raise
+    start_time = time.time()
+    print("run comparing...")
+    list_probe = create_users.make("fingers/probe/crossMatchDB_mini")
+    print("created list of probe...")
+    list_standard = create_users.make("fingers/standard/crossMatchDB_mini")
+    print("created list of standard...")
+
+    pool_size = multiprocessing.cpu_count() * 2
+    pool = multiprocessing.Pool(processes=pool_size)
+    list_results = pool.map(lambda probe: comparing.compare_one_with_n(probe, list_standard), list_probe)
+    # print("list", list(map(lambda res: res.to_string(), list_results)))
+    pool.close()
+    pool.join()
+    print(comparing.fold_results(list_results).to_string())
+    print("%s sec" % (time.time() - start_time))
